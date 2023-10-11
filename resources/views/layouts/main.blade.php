47e239917fba58e7e1492d7a7aa024ea0fdda8a7
“amstrongnpsb” <amstrongpsb@gmail.com>
2023-10-11 16:10:22 +0700

    add app logo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{URL::asset('storage/imgs/application-approved-icon.png')}}" type="image/icon type">
+    <link rel="icon" href="{{URL::asset('appimgs/app-logo.png')}}" type="image/icon type">
     <title>Approval System - {{ $title }}</title>
     <link href="{{URL::asset('css/styles.css')}}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
